a83bf28dc885323734f2dfb5e856047fdd10307d
Bryn McKerracher <brynmckerracher@gmail.com>
2024-08-12 17:22:46 +1200

    Add Frame skeleton

---
+++ Frame.h
@@ -0,0 +1,15 @@
+#ifndef FRAME_H_INCLUDED
+#define FRAME_H_INCLUDED
+
+namespace Noble::Core
+{
+    class Frame
+    {
+        public:
+
+        protected:
+        
+    };
+}
+
+#endif //FRAME_H_INCLUDED
