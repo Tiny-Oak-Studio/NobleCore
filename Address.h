ec27f3a98e1a2dcf28dea5640e67ee51650cc6bc
Bryn McKerracher <brynmckerracher@gmail.com>
2024-08-12 17:54:37 +1200

    Add Address skeleton

---
+++ Address.h
@@ -0,0 +1,18 @@
+#ifndef NOBLE_CORE_ADDRESS_H_INCLUDED
+#define NOBLE_CORE_ADDRESS_H_INCLUDED
+
+#include <stdint.h>
+
+namespace Noble::Core
+{
+    /// @brief Provides translation services for a 16-bit address space.
+    class Address
+    {
+    public:
+        
+    protected:
+
+    };
+}
+
+#endif //ADDRESS_H_INCLUDED
--- Frame.h
+++ Frame.h
@@ -1,15 +1,16 @@
-#ifndef FRAME_H_INCLUDED
-#define FRAME_H_INCLUDED
+#ifndef NOBLE_CORE_FRAME_H_INCLUDED
+#define NOBLE_CORE_FRAME_H_INCLUDED
 
 namespace Noble::Core
 {
+    /// @brief Contains Noble Assembly Language Sequence (NAL) information and translation methods.
     class Frame
     {
-        public:
+    public:
 
-        protected:
-        
+    protected:
+
     };
 }
 
-#endif //FRAME_H_INCLUDED
+#endif //NOBLE_CORE_FRAME_H_INCLUDED
