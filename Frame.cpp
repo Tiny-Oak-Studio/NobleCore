465cf2c60b0cd341bbdf96b5df336c1b2a41acba
Bryn McKerracher <brynmckerracher@gmail.com>
2024-09-17 12:31:19 +1200

    Replace Frame implementation to use runtime frames so that the compiler can define compiler-specific frames.

--- Frame.cpp
+++ Frame.cpp
@@ -2,60 +2,9 @@
 
 namespace Noble::Core
 {
-    void Frame::WriteOp(const Op::Type &op)
+    Frame::~Frame()
     {
-        ops.Add(op);
-    }
-
-    void Frame::WriteAddress(const Address::Single &address)
-    {
-        ops.Add(Translation::AddressToOps(address));
-    }
-
-    void Frame::WriteConstant(const ValueType value)
-    {
-        WriteOp(Op::Code::Constant);
-        WriteAddress(AddConstant(value));
-    }
-
-    Op::Type Frame::ReadOp(const Address::Single& address)
-    {
-        return ops[address];
-    }
-
-    Address::Single Frame::ReadAddress(const Address::Single &address) const
-    {
-        return Translation::OpsToAddress(ops, address);
-    }
-
-    const ValueType& Frame::ReadConstant(const Address::Single address) const
-    {
-        return constants[address];
-    }
-
-    Address::Single Frame::AddConstant(const ValueType value)
-    {
-        constants.Add(value);
-        return constants.Count() - 1;
-    }
-
-    Address::Single Frame::NumOps() const
-    {
-        return ops.Count();
-    }
-
-    Address::Single Frame::NumConstants() const
-    {
-        return constants.Count();
-    }
-
-    const List<Op::Type>& Frame::GetOps() const
-    {
-        return ops;
-    }
-
-    const List<ValueType>& Frame::GetConstants() const
-    {
-        return constants;
+        delete [] ops;
+        delete [] constants;
     }
 }
--- Frame.h
+++ Frame.h
@@ -1,58 +1,32 @@
 #ifndef NOBLE_CORE_FRAME_H_INCLUDED
 #define NOBLE_CORE_FRAME_H_INCLUDED
 
-#include <stdint.h> //Use old header for Arduino compatibility
 #include "Value.h"
-#include "List.h"
 #include "Op.h"
-#include "Translation.h"
-
-using namespace Noble::Core::Runtime;
+#include "Address.h"
 
 namespace Noble::Core
 {
-    /// @brief Contains Noble Assembly Language Sequence (NAL) information and translation methods.
-    class Frame
+    /**
+     * @class Frame
+     * @brief Holds executable Noble Assembly Sequences (NAS) for execution in a runtime environment.
+     */
+    struct Frame
     {
-    public:
-        /// @brief Writes an op to this frame's underlying array
-        void WriteOp(const Op::Type& op);
+        /**
+         * @brief Deletes the ops and constant array.
+         */
+        ~Frame();
 
-        /// @brief Writes an address into the frame's underlying array
-        void WriteAddress(const Address::Single& address);
+        /// @brief Array of Noble Assembly Ops to be executed by a VM.
+        Op::Type* ops = nullptr;
+        /// @brief Number of ops in the array.
+        Address::Single numOps = 0;
 
-        /// @brief Writes the value to the ops array.
-        void WriteConstant(ValueType value);
-
-        /// @brief Returns the op at the specified address
-        [[nodiscard]] Op::Type ReadOp(const Address::Single& address);
-
-        /// @brief Reads ops from the specified address and returns them as an address
-        [[nodiscard]] Address::Single ReadAddress(const Address::Single &address) const;
-
-        /// @brief Reads a constant at the given address.
-        [[nodiscard]] const ValueType& ReadConstant(Address::Single address) const;
-
-        /// @brief Adds a constant to this frame
-        Address::Single AddConstant(ValueType value);
-
-        /// @brief Returns the number of ops currently stored in the ops list
-        [[nodiscard]] Address::Single NumOps() const;
-
-        /// @brief Returns the number of constantly currently stored
-        [[nodiscard]] Address::Single NumConstants() const;
-
-        /// @brief Get a const ref to the ops array
-        [[nodiscard]] const List<Op::Type>& GetOps() const;
-
-        /// @brief Get a const ref to the constants array
-        [[nodiscard]] const List<ValueType>& GetConstants() const;
-    protected:
-        /// @brief Storage for reading/writing ops in the compiler
-        List<Op::Type> ops;
-
-        /// @brief Storage for the constants used in this frame
-        List<ValueType> constants;
+        /// @brief Array of constants used be the NAS in this Frame.
+        Runtime::ValueType* constants = nullptr;
+        /// @brief Number of constants stored in the array.
+        Address::Single numConstants = 0;
     };
 }
 
