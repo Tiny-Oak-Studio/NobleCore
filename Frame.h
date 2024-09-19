fa28e24a2617c45c43f3b2f9bacd461ddd4ee9b9
Bryn McKerracher <brynmckerracher@gmail.com>
2024-09-19 14:18:20 +1200

    Update docs, update Frame docs

--- Frame.h
+++ Frame.h
@@ -9,29 +9,33 @@ namespace Noble::Core
 {
     /**
      * @class Frame
-     * @brief Holds executable Noble Assembly Sequences (NAS) for execution in a runtime environment.
+     * @brief Holds executable Noble Assembly Language (NAL) sequences for execution in a runtime environment.
      */
     struct Frame
     {
         /**
-         * @brief Deletes the ops and constant array.
+         * @brief Deletes all member data arrays.
          */
         ~Frame();
 
         /**
          * @brief Array of Noble Assembly Ops to be executed by a VM.
+         *
+         * This array is created by the compiler and read by the VM.
          */
         Op::Type* ops = nullptr;
 
-        /// @brief Number of ops stored in the array.
+        /// @brief Number of ops stored in the ops array.
         Address::Single numOps = 0;
 
         /**
-         * @brief Array of constants used be the NAS in this Frame.
+         * @brief Array of constants to be used by this Frame.
+         *
+         * This array is created by the compiler and read by the VM.
          */
         Runtime::ValueType* constants = nullptr;
 
-        /// @brief Number of constants stored in the array.
+        /// @brief Number of constants stored in the constants array.
         Address::Single numConstants = 0;
     };
 }
