21105b15ebda8dc2e386e335c8997063c28aefa8
BrynMckerracher <brynmckerracher@gmail.com>
2025-04-17 18:15:29 +1200

    Add IsFalsey() function to value module

--- Value.cpp
+++ Value.cpp
@@ -41,7 +41,7 @@ namespace Noble::Core::Runtime
         return value == TrueValue || value == FalseValue;
     }
 
-    bool IsFalsey(ValueType value)
+    bool IsFalsey(const ValueType value)
     {
         return IsNull(value) or (IsBool(value) and !ToBool(value));
     }
